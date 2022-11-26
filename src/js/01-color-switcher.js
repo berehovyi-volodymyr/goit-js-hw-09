8306b773e08a041a0343d0c4c9d700e3b0780bb1
berehovyi-volodymyr <106471456+berehovyi-volodymyr@users.noreply.github.com>
2022-11-26 10:57:45 +0200

    1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const ref = {
+    btnStart: document.querySelector('button[data-start]'),
+    btnStop: document.querySelector('button[data-stop]'),
+    body: document.querySelector('body')
+}
+
+ref.btnStart.addEventListener('click', onStart)
+ref.btnStop.addEventListener('click', onStop)
+
+function onStart() {
+    timerId  = setInterval(() => {
+        ref.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    ref.btnStart.setAttribute("disabled", "disabled")
+}
+
+function onStop() {
+    ref.btnStart.removeAttribute("disabled")
+    clearInterval(timerId)
+}
